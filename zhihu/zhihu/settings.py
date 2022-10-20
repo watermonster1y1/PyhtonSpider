6082df110b3a831bad47a62312f3ab503f902fec
watermonster1y1 <1272155633@qq.com>
2022-10-20 11:03:32 +0800

    获取热榜
    获取热榜，以字典形式存储数据

--- zhihu/zhihu/settings.py
+++ zhihu/zhihu/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'zhihu.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'zhihu (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
 
 # Obey robots.txt rules
 # ROBOTSTXT_OBEY = True
--- zhihu/zhihu/spiders/ZhihuSpider.py
+++ zhihu/zhihu/spiders/ZhihuSpider.py
@@ -1,10 +1,9 @@
 import scrapy
-
+import json
 
 class ZhihuspiderSpider(scrapy.Spider):
     name = 'ZhihuSpider'
     allowed_domains = ['www.zhihu.com']
-    start_urls = ['http://www.zhihu.com/']
-
+    start_urls = ['https://www.zhihu.com/api/v4/creators/rank/hot?domain=0&limit=100&offset=0&period=hour']
     def parse(self, response):
-        pass
+        data = json.loads(response.text)
