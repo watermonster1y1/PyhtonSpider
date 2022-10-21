8f3f5744aff5699adf6c39326bb3c08dc50f1b4f
watermonster1y1 <1272155633@qq.com>
2022-10-21 21:02:57 +0800

    寄
    寄寄寄寄寄寄寄寄寄寄寄寄

--- bili/bili/spiders/BiliSpider.py
+++ bili/bili/spiders/BiliSpider.py
@@ -4,7 +4,10 @@ import scrapy
 class BilispiderSpider(scrapy.Spider):
     name = 'BiliSpider'
     allowed_domains = ['space.bilibili.com']
-    start_urls = ['http://space.bilibili.com/']
-
+    start_urls = ['https://space.bilibili.com/']
+    uid_A = input("输入uid")
+    uid_B = input("输入uid")
+    base = 'https://space.bilibili.com/'
+    tail = '/fans/follow'
     def parse(self, response):
         pass
